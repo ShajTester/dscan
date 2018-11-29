f30c6485600be9202bd8b2ab6685eff33f0ca96a
ShajTester <andrey-shaj@narod.ru>
2018-11-29 14:49:28 +0300

    yocto bb file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 
 
 #include <iostream>
+#include <string>
 
 
 #include "args.hxx"
@@ -15,6 +16,7 @@ int main(int argc, char const *argv[])
 	args::ArgumentParser parser("R-BD-E5R-V4 i2c devices scanner", "This goes after the options");
     args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
     args::Flag ver(parser, "ver", "version ", {'v', "version"});
+    args::ValueFlag<std::string> conf(parser, "conf", "Config file name", {'c', "conf"});
 
     try
     {
@@ -43,6 +45,11 @@ int main(int argc, char const *argv[])
     {
     	std::cout << "R-BD-E5R-V4 i2c devices scanner\nversion " << VER << std::endl;
     	return 0;
+    }
+
+    if(conf)
+    {
+    	std::cout << "Config file name: " << args::get(conf) << std::endl;
     }
 
     return 0;
